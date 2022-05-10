187773d997ca5db2608bd12e3e4e6552dbfa6541
Patrick Chung <patrick.chung@onlydoge.net>
2022-05-09 17:46:48 -0700

    Added formatting template for all repo scripts
      Added formatting template for all repo scripts

---
+++ functionApply.py
@@ -0,0 +1,25 @@
+#functionApply.py
+#2022-05-09
+#Script to highlight all the types of functions that ezdxf library has.
+
+'''
+                     .__
+______ ______   ____ |  |__  __ __  ____    ____
+\____ \\____ \_/ ___\|  |  \|  |  \/    \  / ___\
+|  |_> >  |_> >  \___|   Y  \  |  /   |  \/ /_/  >
+|   __/|   __/ \___  >___|  /____/|___|  /\___  /
+|__|   |__|        \/     \/           \//_____/
+
+'''
+
+#cyberp200@rocketmail.com (github)
+#ppchung@gmx.com
+#562-480-2114
+
+#imports
+
+#vars
+
+#main
+
+#functions
